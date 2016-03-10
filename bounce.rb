f0516cfaae313f0bff7abc9526b4988fdc8f357a
Naghmeh <naghmehd@gmail.com>
2016-03-09 22:09:16 -0600

    updated

--- bounce.rb
+++ bounce.rb
@@ -1,17 +1,22 @@
 
 
 def check_age()
+  loop do
   print "How old are you?"
   age = gets.chomp
-  case
-  when 0..17
-    puts "If I am younger than 18, I can do nothing."
-  when 18..20
-    puts "If I am older than 18, I can vote. " + "If I am older than 18, I can vote."
-  when 20..24
-    puts "If I am older than 21, I can drink."
-  else 25..100
-    puts "If I am older than 25, I can rent a car."
+  break if age.to_str == "stop"
+    case age.to_i
+    when 0..17
+      puts "You are younger than 18, I can do nothing."
+    when 18..20
+      puts "You are older than 18, you can vote. "
+    when 20..24
+      puts "You are older than 21, I can drink."
+    when 25..100
+      puts "You are older than 25, I can rent a car."
+    when 'stop'
+      break
+    end
   end
 end
 
